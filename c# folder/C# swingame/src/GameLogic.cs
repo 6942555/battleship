94b34d58754b4c6ebc9d6263a01db19f414ee60f
Ezmariel <d.spivak@optusnet.com.au>
2015-10-14 13:00:01 +1100

    no message

--- c# folder/C# swingame/src/GameLogic.cs
+++ c# folder/C# swingame/src/GameLogic.cs
@@ -2,8 +2,8 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
+using SwinGameSDK;
 static class GameLogic
 {
 	public static void Main()
