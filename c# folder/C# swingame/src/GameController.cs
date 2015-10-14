f374b189b7a44b2f25ef8c77f1df0673cdbfb42f
Ezmariel <d.spivak@optusnet.com.au>
2015-10-14 13:16:54 +1100

    no message

--- c# folder/C# swingame/src/GameController.cs
+++ c# folder/C# swingame/src/GameController.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using SwinGameSDK;
 using static DeploymentController;
 using static UtilityFunctions;
 using static GameResources;
