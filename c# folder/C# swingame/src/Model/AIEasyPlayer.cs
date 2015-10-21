bd1fddafa77bf186dc0faee2c8a390a0a62fbc34
Ezmariel <d.spivak@optusnet.com.au>
2015-10-21 18:23:23 +1100

    no message

--- c# folder/C# swingame/src/Model/AIEasyPlayer.cs
+++ c# folder/C# swingame/src/Model/AIEasyPlayer.cs
@@ -9,19 +9,6 @@ using System.Diagnostics;
 /// </summary>
 public class AIEasyPlayer : AIPlayer
 {
-    /// <summary>
-    /// Private enumarator for AI states. currently there are two states,
-    /// the AI can be searching for a ship, or if it has found a ship it will
-    /// target the same ship
-    /// </summary>
-    private enum AIStates
-    {
-        Searching
-    }
-
-    private AIStates _CurrentState = AIStates.Searching;
-
-    private Stack<Location> _Targets = new Stack<Location>();
     public AIEasyPlayer(BattleShipsGame controller) : base(controller)
     {
     }
@@ -37,22 +24,24 @@ public class AIEasyPlayer : AIPlayer
     {
         do
         {
-            //check which state the AI is in and uppon that choose which coordinate generation
-            //method will be used.
-            switch (_CurrentState)
-            {
-                case AIStates.Searching:
-                    SearchCoords(ref row, ref column);
-                    break;
-                case AIStates.TargetingShip:
-                    TargetCoords(ref row, ref column);
-                    break;
-                default:
-                    throw new ApplicationException("AI has gone in an imvalid state");
-            }
+            SearchCoords(ref row, ref column);
         } while ((row < 0 || column < 0 || row >= EnemyGrid.Height || column >= EnemyGrid.Width || EnemyGrid[row, column] != TileView.Sea));
         //while inside the grid and not a sea tile do the search
     }
+
+    /// <summary>
+	/// SearchCoords will randomly generate shots within the grid as long as its not hit that tile already
+	/// </summary>
+	/// <param name="row">the generated row</param>
+	/// <param name="column">the generated column</param>
+	private void SearchCoords(ref int row, ref int column)
+    {
+        row = _Random.Next(0, EnemyGrid.Height);
+        column = _Random.Next(0, EnemyGrid.Width);
+    }
+
+    protected override void ProcessShot(int row, int col, AttackResult result)
+    { }
 }
 
  
